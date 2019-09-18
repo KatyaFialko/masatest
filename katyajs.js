65a7a3eb10109dba9b97424ebe6d75a56868fc41
KatyaFialko <the_very_kat@mail.ru>
2019-09-18 19:00:39 +0300

    kat

---
+++ katyajs.js
@@ -0,0 +1,24 @@
+let myAge;
+var myName;
+
+myName = "Katya";
+
+
+console.log(typeof myName) //string
+console.log(typeof myAge) //number
+
+var array = [1,2,false,"232323",5,6,7,8,9]
+array[4567] // underfined
+array[1]
+
+var user = {
+    name: "Katya",
+    surname: "Fialko",
+    friends: ["Amber", "Ann", 
+              {
+              name: "vkdfdjkj",
+              surname: "jdhjdfh"
+              }]
+}
+
+
